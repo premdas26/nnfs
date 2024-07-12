09201ca241c59400f2f05689d8be534b666b4cc3
Premal Patel <premalp@udel.edu>
2024-07-11 19:29:09 -0700

    add in regularization

--- loss/loss.py
+++ loss/loss.py
@@ -4,3 +4,22 @@ class Loss:
     def calculate(self, output, y):
         sample_losses = self.forward(output, y)
         return np.mean(sample_losses)
+    
+    def regularization_loss(self, layer):
+        regularization_loss = 0
+        
+        if layer.weight_regularizer_l1 > 0:
+            regularization_loss += layer.weight_regularizer_l1 * np.sum(np.abs(layer.weights))
+            
+        if layer.weight_regularizer_l2 > 0:
+            regularization_loss += layer.weight_regularizer_l2 * np.sum(layer.weights ** 2)
+            
+        if layer.bias_regularizer_l1 > 0:
+            regularization_loss += layer.bias_regularizer_l1 * np.sum(np.abs(layer.biases))
+            
+        if layer.bias_regularizer_l2 > 0:
+            regularization_loss += layer.bias_regularizer_l2 * np.sum(layer.biases ** 2)
+
+        return regularization_loss
+            
+        
--- network.py
+++ network.py
@@ -8,33 +8,36 @@ from nnfs.datasets import spiral_data, vertical_data
 
 nnfs.init()
 
-x, y = spiral_data(samples=100, classes=3)
+x, y = spiral_data(samples=1000, classes=3)
 
-dense1 = DenseLayer(2, 64)
+dense1 = DenseLayer(2, 512, weight_regularizer_l2=5e-4, bias_regularizer_l2=5e-4)
 activation1 = ActivationReLU()
 
-dense2 = DenseLayer(64, 3)
+dense2 = DenseLayer(512, 3)
 loss_activation = SoftmaxClassifier()
 
 # optimizer = SGDOptimizer(decay=1e-3, momentum=0.88)
 # optimizer = AdaOptimizer(decay=1e-4)
 # optimizer = RMSPropOptimizer(decay=1e-5, learning_rate=0.02, rho=0.999)
-optimizer = AdamOptimizer(learning_rate=0.05, decay=5e-7)
+optimizer = AdamOptimizer(learning_rate=0.02, decay=5e-7)
 
 for epoch in range(10001):
     dense1.forward(x)
     activation1.forward(dense1.output)
 
     dense2.forward(activation1.output)
-    loss = loss_activation.forward(dense2.output, y)
-
+    data_loss = loss_activation.forward(dense2.output, y)
+    regularization_loss = loss_activation.loss.regularization_loss(dense1) + \
+                            loss_activation.loss.regularization_loss(dense2)
+    loss = data_loss + regularization_loss
+    
     predictions = np.argmax(loss_activation.output, axis=1)
     if len(y.shape) == 2:
         y = np.argmax(y, axis=1)
     accuracy = np.mean(predictions == y)
     
     if not epoch % 1000:
-        print(f'epoch: {epoch}, accuracy: {accuracy:.3f}, loss: {loss:.3f}, lr: {optimizer.current_learning_rate}')
+        print(f'epoch: {epoch}, accuracy: {accuracy:.3f}, loss: {loss:.3f} (data_loss: {data_loss}, reg_loss: {regularization_loss}), lr: {optimizer.current_learning_rate}')
 
     loss_activation.backward(loss_activation.output, y)
     dense2.backward(loss_activation.dinputs)
@@ -52,10 +55,10 @@ dense1.forward(x_test)
 activation1.forward(dense1.output)
 
 dense2.forward(activation1.output)
-loss = loss_activation.forward(dense2.output, y)
+loss = loss_activation.forward(dense2.output, y_test)
 
 predictions = np.argmax(loss_activation.output, axis=1)
-if len(y.shape) == 2:
-    y = np.argmax(y, axis=1)
-accuracy = np.mean(predictions == y)
+if len(y_test.shape) == 2:
+    y_test = np.argmax(y_test, axis=1)
+accuracy = np.mean(predictions == y_test)
 print(f'validation, accuracy: {accuracy:.3f}, loss: {loss:.3f}')
